83de7d103e726258817e55db29db0724f7a7c961
Pooja Jadhav <jadhavpoojasopan@gmail.com>
2023-03-27 18:51:54 +0530

    qwerty

--- src/main/java/Bike.java
+++ src/main/java/Bike.java
@@ -18,6 +18,32 @@ public class Bike {
         speed += increment;
     }
 
+	public static boolean writeStreamToFile(InputStream stream, File file){
+		FileOutputStream out = null;
+		try{
+			out = new FileOutputStream(file);
+			byte[] buffer = new byte[4096];
+			int byeRead;
+			while (byteRead = stream.read(buffer) != -1){
+				out.write(buffer,0,byeRead);
+			}
+			out.close();
+			return true;	
+		} catch(Exception e){
+			e.printStackStrace();
+			return false;
+		} finally{
+			if (out !=null){
+				try{
+					out.close();
+				} catch(IOException e){
+					e.printStackStrace();
+				}
+				out = null;
+			}
+		}
+		
+	}
     public String toString()
     {
         return ("No of gears are " + gear + "\n"
